2aa6103d0265828d0f8b629fc1d72ae5da164250
unknown <alia.azarian@gmail.com>
2020-12-04 23:25:24 +0000

    added initial state and api fetch to the  cart component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { NavBar, Products } from './components'
 const App = () => {
 
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
 
     const fetchProducts = async() => {
         // const response = await commerce.products.list()
@@ -14,11 +15,20 @@ const App = () => {
         setProducts(data)
     }
 
+    const fetchCart = async () => {
+        // const response = await commerce.cart.retrieve()
+        // const cart = await commerce.cart.retrieve()
+        // setCart(cart)
+        setCart(await commerce.cart.retrieve())
+    }
+
     useEffect(() => {
         fetchProducts()
+        fetchCart()
     }, [])
 
     // console.log(products)
+    console.log(cart)
 
     return (
         <div>
