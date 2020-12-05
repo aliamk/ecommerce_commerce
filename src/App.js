efe69c6b79eb135cbeae574dbac8cbcc695fad92
unknown <alia.azarian@gmail.com>
2020-12-05 13:56:22 +0000

    React router added to App.js in order to switch between the products page and cart page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce'
 import { NavBar, Products, Cart } from './components'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
 
@@ -37,11 +38,19 @@ const App = () => {
     console.log(cart)
 
     return (
-        <div>
-            <NavBar totalItems={cart.total_items} />
-            {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
-            <Cart cart={cart} />
-        </div>
+        <Router>
+            <div>
+                <NavBar totalItems={cart.total_items} />
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart}/>
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart cart={cart} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
