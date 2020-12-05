75a7c2b7825168344a0c256c111e580d7811f4b6
unknown <alia.azarian@gmail.com>
2020-12-05 19:38:11 +0000

    Add three option parameters for the three types of shipping options undr the address form, initialise the useState hooks for each, create a function fetchShippingCountries that connects to Commerce.js' api for its list of countries

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core'
 import { useForm, FormProvider } from 'react-hook-form'
+
+import { commerce } from '../../lib/commerce'
 
 import FormInput from './FormInput'
 
 
 const AddressForm = () => {
+    // These hooks are needed to set the shipping options that correspond to the ones in Commerce.js' dashboard
+    // Each option depends on another: option => subdivision => country
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
     // Hook from react-hook-form which is spread into FormProvider below
     const methods = useForm()
+
+    // Setup token in Checkout.jsx
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        // const response = await commerce.services.localeListShippingCountries(checkoutTokenId)
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId)
+        setShippingCountries(countries)
+    }
 
     return (
         <>
@@ -21,6 +38,28 @@ const AddressForm = () => {
                         <FormInput required name='email' label='Email' />
                         <FormInput required name='city' label='City' />
                         <FormInput required name='zipPostalCode' label='Zip/Postal Code' />
+
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={ } fullWidth onChange={ }>
+                                <MenuItem key={ } value={ }>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value={ } fullWidth onChange={ }>
+                                <MenuItem key={ } value={ }>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value={ } fullWidth onChange={ }>
+                                <MenuItem key={ } value={ }>Select Me</MenuItem>
+                            </Select>
+                        </Grid>
+
                     </Grid>
                 </form>
             </FormProvider>
